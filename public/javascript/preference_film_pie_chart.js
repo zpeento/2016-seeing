1bb3ec72f7b0aac2163226f508b9e1d9a5a4ab3b
iceiceice <509887158@qq.com>
2017-02-13 01:57:43 +0800

    [update]用户画像页-偏好电影部分

---
+++ public/javascript/preference_film_pie_chart.js
@@ -0,0 +1,40 @@
+//views/single_film/persona.html 偏好电影——饼图
+
+$(function(){
+	var option = {
+	    title : {
+	    },
+	    tooltip : {
+	        trigger: 'item',
+	        formatter: "{a} <br/>{b} : {c} ({d}%)"
+	    },
+	    series : [
+	        {
+	            name: '访问来源',
+	            type: 'pie',
+	            radius : '55%',
+	            center: ['50%', '40%'],
+	            data:[
+	                {value:335, name:'直接访问'},
+	                {value:310, name:'邮件营销'},
+	                {value:234, name:'联盟广告'},
+	                {value:135, name:'视频广告'},
+	                {value:1548, name:'搜索引擎'}
+	            ],
+	            itemStyle: {
+	                emphasis: {
+	                    shadowBlur: 10,
+	                    shadowOffsetX: 0,
+	                    shadowColor: 'rgba(0, 0, 0, 0.5)'
+	                }
+	            }
+	        }
+	    ]
+	};
+
+	var myChart = echarts.init(document.getElementById('preference_film_pie'));
+
+    myChart.setOption(option);
+
+    console.log('0')
+})
