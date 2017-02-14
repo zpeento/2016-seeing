290c3103b4bce57a494fe4988e1e4239a351441d
iceiceice <509887158@qq.com>
2017-02-15 00:45:11 +0800

    [update]修改人物评价页的力导图的样式

--- public/javascript/character_evaluation_force_chart.js
+++ public/javascript/character_evaluation_force_chart.js
@@ -28,20 +28,13 @@ var option = {
                 linkStyle: {
                     type: 'curve',
                     color:'#2aaa90',
-                    width:2
                 }
                 
             },
 
             emphasis:{
-                label : {show : true,
-                         textStyle:{
-                             fontWeight:900 }
-                         },
                 nodeStyle : {
-                    color:'#f1ab66',
-                    borderColor : 'rgba(255,255,255,1)',
-                    borderWidth : 3
+                    borderWidth : 0
                 }
             }
         },
@@ -56,9 +49,8 @@ var option = {
                 itemStyle:{
                     normal:{
                         color:'#24b1b6',
-                        borderColor:'#18838b',
-                        borderWidth:6,
-                        borderWidth : 0,
+                        borderColor:'#2d7b7e',
+                        borderWidth:8,
                         label : {
                             show : true,
                             textStyle:{
@@ -69,15 +61,9 @@ var option = {
                     },
 
                     emphasis:{
-                        borderWidth:6,
-                        borderColor:'#fff',
-                        label : {
-                            show : true,
-                            textStyle:{
-                                fontSize:15,
-                                fontWeight:700
-                            }
-                        }
+                        borderWidth:0,
+                        borderColor:'#24b1b6'
+                        // borderColor:'#fff',
                     }
                 }
             }, 
@@ -86,7 +72,7 @@ var option = {
 
             {
                 name : '1',
-                symbolSize:25,
+                symbolSize:15,
                 itemStyle:{
                     normal:{
                         color:'#2aaa90',
@@ -120,13 +106,12 @@ var option = {
 
                     emphasis:{
                         borderWidth:4,
-                        borderColor:'#fff',
-
+                        borderColor:'#637e76',
+                        color:'#f1ab66',
                         label : {
                             show : true,
                             textStyle:{
-                                fontSize:13,
-                                fontWeight:700
+                                fontSize:13
                             }
                         }
                     }
@@ -135,19 +120,19 @@ var option = {
         ],
 
         nodes : [
-            {id:0,category:0,name:'film',label:'但丁密码',ignore:false,flag:true,initial:[313,360],fixX:true,fixY:true,card:'film'},
-            {id:1,category:1,name:'director',label:'导演',ignore:true,flag:true,initial:[380,270],fixX:true,fixY:true},
-            {id:2,category:1,name:'scriptwriter',label:'编剧',ignore:true,flag:true,initial:[380,430],fixX:true,fixY:true},
-            {id:3,category:1,name:'role0',label:'演员',ignore:true,flag:true,initial:[235,265],fixX:true,fixY:true},
-            {id:4,category:1,name:'role1',label:'演员',ignore:true,flag:true,initial:[230,452],fixX:true,fixY:true},
-            {id:5,category:1,name:'role2',label:'演员',ignore:true,flag:true,initial:[440,160],fixX:true,fixY:true}, 
-            {id:6,category:1,name:'role3',label:'演员',ignore:true,flag:true,initial:[480,230],fixX:true,fixY:true},
-            {id:7,category:2,name:'0',label:'朗·霍华德',ignore:true,flag:true,card:'card',initial:[500,390],fixX:true,fixY:true},
-            {id:8,category:2,name:'1',label:'大卫凯普',ignore:true,flag:true,card:'card',initial:[480,495],fixX:true,fixY:true},
-            {id:9,category:2,name:'2',label:'汤姆·汉克斯',ignore:true,flag:true,card:'card',initial:[435,545],fixX:true,fixY:true},
-            {id:10,category:2,name:'3',label:'汤姆·汉克斯',ignore:true,flag:true,card:'card',initial:[220,140],fixX:true,fixY:true},
-            {id:11,category:2,name:'4',label:'汤姆·汉克斯',ignore:true,flag:true,card:'card',initial:[170,180],fixX:true,fixY:true},
-            {id:12,category:2,name:'5',label:'刘德华',ignore:true,flag:true,card:'card',initial:[130,230],fixX:true,fixY:true}
+            {id:0,category:0,name:'film',label:'但丁密码',ignore:false,flag:true,initial:[355,245],fixX:true,fixY:true,card:'film'},
+            {id:1,category:1,name:'director',label:'导演',ignore:false,flag:true,initial:[430,155],fixX:true,fixY:true},
+            {id:2,category:1,name:'scriptwriter',label:'编剧',ignore:false,flag:true,initial:[310,140],fixX:true,fixY:true},
+            {id:3,category:1,name:'role0',label:'演员',ignore:false,flag:true,initial:[230,225],fixX:true,fixY:true},
+            {id:4,category:1,name:'role1',label:'演员',ignore:false,flag:true,initial:[270,342],fixX:true,fixY:true},
+            {id:5,category:1,name:'role2',label:'演员',ignore:false,flag:true,initial:[400,367],fixX:true,fixY:true}, 
+            {id:6,category:1,name:'role3',label:'演员',ignore:false,flag:true,initial:[475,255],fixX:true,fixY:true},
+            {id:7,category:2,name:'0',label:'朗·霍华德',ignore:false,flag:true,card:'card',initial:[435,55],fixX:true,fixY:true},
+            {id:8,category:2,name:'1',label:'大卫凯普',ignore:false,flag:true,card:'card',initial:[225,90],fixX:true,fixY:true},
+            {id:9,category:2,name:'2',label:'汤姆·汉克斯',ignore:false,flag:true,card:'card',initial:[155,290],fixX:true,fixY:true},
+            {id:10,category:2,name:'3',label:'汤姆·汉克斯',ignore:false,flag:true,card:'card',initial:[260,425],fixX:true,fixY:true},
+            {id:11,category:2,name:'4',label:'汤姆·汉克斯',ignore:false,flag:true,card:'card',initial:[482,415],fixX:true,fixY:true},
+            {id:12,category:2,name:'5',label:'刘德华',ignore:false,flag:true,card:'card',initial:[565,205],fixX:true,fixY:true}
         ],
 
         links : [ 
@@ -159,7 +144,6 @@ var option = {
             {source : 6,target : 0}, 
             {source : 7,target :1}, 
             {source : 8,target : 2}, 
-            // {source : 9,target : 2}, 
             {source : 9,target : 3}, 
             {source : 10,target : 4}, 
             {source : 11,target : 5},
@@ -169,85 +153,85 @@ var option = {
     }]
 };
 
-(function(){
-    // alert(option.series[0].links.length)
-    //生成角色一级节点
-    // 角色一级节点备用节点位置（一般4个，我预备到6个）
-    var nodeSetPrep1 = [[125,420],[140,480],[176,526],[220,570],[510,580],[540,640]];
-    // 角色二级节点备用节点位置（一般4个，我预备到6个）
-    var nodeSetPrep2 = [[35,380],[50,475],[90,545],[150,605],[180,595],[210,580]];
-    // 发行公司节点备用节点位置
-    var nodeSetPrep3 = [[250,50],[150,63],[350,75]];
-    // 导演节点备用节点位置
-    var nodeSetPrep4 = [[110,118],[270,33],[120,83]];
-    // 编剧节点备用节点位置
-    var nodeSetPrep5 = [[50,210],[120,160],[100,290]];
-    var linksSet = option.series[0].links;
-    var nodesSet = option.series[0].nodes;
-    // 用于存放角色一级节点的id的数组
-    var roleList1 = [];
-    // 用于存放角色二级节点的id的数组
-    var roleList2 = [];
-    // 用于存放发行公司的id的数组
-    var companyList = [];
-    // 用于存放导演的id的数组
-    var directorList = [];
-    // 用于存放编剧的id的数组
-    var scriptwriterList = [];
+// (function(){
+//     // alert(option.series[0].links.length)
+//     //生成角色一级节点
+//     // 角色一级节点备用节点位置（一般4个，我预备到6个）
+//     var nodeSetPrep1 = [[125,420],[140,480],[176,526],[220,570],[510,580],[540,640]];
+//     // 角色二级节点备用节点位置（一般4个，我预备到6个）
+//     var nodeSetPrep2 = [[35,380],[50,475],[90,545],[150,605],[180,595],[210,580]];
+//     // 发行公司节点备用节点位置
+//     var nodeSetPrep3 = [[250,50],[150,63],[350,75]];
+//     // 导演节点备用节点位置
+//     var nodeSetPrep4 = [[110,118],[270,33],[120,83]];
+//     // 编剧节点备用节点位置
+//     var nodeSetPrep5 = [[50,210],[120,160],[100,290]];
+//     var linksSet = option.series[0].links;
+//     var nodesSet = option.series[0].nodes;
+//     // 用于存放角色一级节点的id的数组
+//     var roleList1 = [];
+//     // 用于存放角色二级节点的id的数组
+//     var roleList2 = [];
+//     // 用于存放发行公司的id的数组
+//     var companyList = [];
+//     // 用于存放导演的id的数组
+//     var directorList = [];
+//     // 用于存放编剧的id的数组
+//     var scriptwriterList = [];
 
-    for(var i = 0; i<linksSet.length; i++){
+//     for(var i = 0; i<linksSet.length; i++){
 
-        if(linksSet[i].target==4){
-            roleList1.push(linksSet[i].source);
-        }
-        if(linksSet[i].target==10){
-            companyList.push(linksSet[i].source);
-        }
-        if(linksSet[i].target==11){
-            directorList.push(linksSet[i].source);
-        }
-        if(linksSet[i].target==12){
-            scriptwriterList.push(linksSet[i].source);
-        }
+//         if(linksSet[i].target==4){
+//             roleList1.push(linksSet[i].source);
+//         }
+//         if(linksSet[i].target==10){
+//             companyList.push(linksSet[i].source);
+//         }
+//         if(linksSet[i].target==11){
+//             directorList.push(linksSet[i].source);
+//         }
+//         if(linksSet[i].target==12){
+//             scriptwriterList.push(linksSet[i].source);
+//         }
 
-    }
+//     }
 
-    for(var i = 0; i < roleList1.length; i++){
-        nodesSet[roleList1[i]].initial = nodeSetPrep1[i];
-        nodesSet[roleList1[i]].fixX = true;
-        nodesSet[roleList1[i]].fixY = true;
-        for(var j = 0; j < linksSet.length;j++){
-            if(roleList1[i]==linksSet[j].target){
-                roleList2.push(linksSet[j].source)
-            }
-        }
-    }
+//     for(var i = 0; i < roleList1.length; i++){
+//         nodesSet[roleList1[i]].initial = nodeSetPrep1[i];
+//         nodesSet[roleList1[i]].fixX = true;
+//         nodesSet[roleList1[i]].fixY = true;
+//         for(var j = 0; j < linksSet.length;j++){
+//             if(roleList1[i]==linksSet[j].target){
+//                 roleList2.push(linksSet[j].source)
+//             }
+//         }
+//     }
 
-    for(var i = 0; i < roleList2.length;i++){
-        nodesSet[roleList2[i]].initial = nodeSetPrep2[i];
-        nodesSet[roleList2[i]].fixX = true;
-        nodesSet[roleList2[i]].fixY = true;
-    }
+//     for(var i = 0; i < roleList2.length;i++){
+//         nodesSet[roleList2[i]].initial = nodeSetPrep2[i];
+//         nodesSet[roleList2[i]].fixX = true;
+//         nodesSet[roleList2[i]].fixY = true;
+//     }
 
 
-    for(var i = 0 ; i < companyList.length ; i++){
-        nodesSet[companyList[i]].initial = nodeSetPrep3[i];
-        nodesSet[companyList[i]].fixX = true;
-        nodesSet[companyList[i]].fixY = true;
-    }
+//     for(var i = 0 ; i < companyList.length ; i++){
+//         nodesSet[companyList[i]].initial = nodeSetPrep3[i];
+//         nodesSet[companyList[i]].fixX = true;
+//         nodesSet[companyList[i]].fixY = true;
+//     }
 
-    for(var i = 0 ; i < directorList.length ; i++){
-        nodesSet[directorList[i]].initial = nodeSetPrep4[i];
-        nodesSet[directorList[i]].fixX = true;
-        nodesSet[directorList[i]].fixY = true;
-    }
+//     for(var i = 0 ; i < directorList.length ; i++){
+//         nodesSet[directorList[i]].initial = nodeSetPrep4[i];
+//         nodesSet[directorList[i]].fixX = true;
+//         nodesSet[directorList[i]].fixY = true;
+//     }
 
-    for(var i = 0 ; i < scriptwriterList.length ; i++){
-        nodesSet[scriptwriterList[i]].initial = nodeSetPrep5[i];
-        nodesSet[scriptwriterList[i]].fixX = true;
-        nodesSet[scriptwriterList[i]].fixY = true;
-    }
-})()
+//     for(var i = 0 ; i < scriptwriterList.length ; i++){
+//         nodesSet[scriptwriterList[i]].initial = nodeSetPrep5[i];
+//         nodesSet[scriptwriterList[i]].fixX = true;
+//         nodesSet[scriptwriterList[i]].fixY = true;
+//     }
+// })()
 
 myChart.setOption(option);
 
