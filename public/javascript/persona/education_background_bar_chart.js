1cf8b09d6b28ad81a0df4eca8353b515854d7ea7
iceiceice <509887158@qq.com>
2017-02-15 14:29:29 +0800

    [update]用户画像页柱状图等样式修改 天啊echarts官网好卡

---
+++ public/javascript/persona/education_background_bar_chart.js
@@ -0,0 +1,83 @@
+//views/single_film/persona.html 背景特征-教育背景折线图
+
+$(function(){
+
+	var option = {
+	    color: ['#3dffff'],
+	    tooltip : {
+	        trigger: 'axis',
+	        axisPointer : {            // 坐标轴指示器，坐标轴触发有效
+	            type : 'shadow'        // 默认为直线，可选为：'line' | 'shadow'
+	        }
+	    },
+	    grid: {
+	        left: '3%',
+	        right: '4%',
+	        bottom: '3%',
+	        containLabel: true
+	    },
+	    xAxis : [
+	        {
+	            type : 'category',
+	            data : ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+	            axisLabel: {
+                    show: true,
+                    textStyle: {
+                        color: '#fff'
+                    }
+                },
+	            axisTick: {
+	                alignWithLabel: true
+	            },
+	            axisLine: {
+	            	lineStyle: {
+	            		color:'#3dffff'
+	            	}
+	            },
+	            splitLine: {
+	            	show:false
+	            }
+	        }
+	    ],
+	    yAxis : [
+	        {
+	            type : 'value',
+	            axisLabel: {
+                    show: true,
+                    textStyle: {
+                        color: '#fff'
+                    }
+                },
+                axisLine: {
+	            	lineStyle: {
+	            		color:'none'
+	            	}
+	            },
+	            splitLine: {
+	            	show:false
+	            }
+	        }
+	    ],
+	    series : [
+	        {
+	            name:'直接访问',
+	            type:'bar',
+	            barWidth: '20%',
+	            data:[10, 52, 200, 334, 390, 330, 220],
+	            itemStyle : { 
+	            	normal: {
+	            		label : {
+	            			color: '#fff',
+	            			show: true, 
+	            			position: 'top'
+	            		}
+	            	}
+	            }
+	        }
+	    ]
+	};
+
+	var myChart = echarts.init(document.getElementById('education_background_line'));
+
+    myChart.setOption(option);
+})
